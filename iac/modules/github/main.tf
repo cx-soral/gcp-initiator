1fc4373d1f9772fd8ad00d41b2cd00fbcb19390b
X-I-A <develop@x-i-a.com>
2024-02-11 12:07:42 +0100

    feat: dev-sit-prd stack support

--- iac/modules/github/main.tf
+++ iac/modules/github/main.tf
@@ -25,3 +25,52 @@ resource "github_repository_environment" "environments" {
   environment         = each.value
   repository          = var.repository_name
 }
+
+data "google_iam_workload_identity_pool_provider" "pool_provider" {
+  for_each = toset(var.env_list)
+
+  provider = google-beta
+  project = "${var.project_prefix}${each.value}"
+  workload_identity_pool_id          = "github-pool-${var.repository_name}"
+  workload_identity_pool_provider_id = "github-provider-${var.repository_name}"
+}
+
+data "google_service_account" "github_provider_sa" {
+  for_each = toset(var.env_list)
+
+  project      = "${var.project_prefix}${each.value}"
+  account_id   = "wip-${var.repository_name}-sa"
+}
+
+resource "github_actions_environment_secret" "project_id" {
+  for_each = toset(var.env_list)
+
+  repository       = var.repository_name
+  environment      = each.value
+  secret_name      = "PROJECT_ID"
+  plaintext_value  = "${var.project_prefix}${each.value}"
+
+  depends_on = [github_repository.app-repository]
+}
+
+resource "github_actions_environment_secret" "secret_wip_name" {
+  for_each = toset(var.env_list)
+
+  repository       = var.repository_name
+  environment      = each.value
+  secret_name      = "SECRET_WIP_NAME"
+  plaintext_value  = data.google_iam_workload_identity_pool_provider.pool_provider[each.key].name
+
+  depends_on = [github_repository.app-repository]
+}
+
+resource "github_actions_environment_secret" "provider_sa_email" {
+  for_each = toset(var.env_list)
+
+  repository       = var.repository_name
+  environment      = each.value
+  secret_name      = "PROVIDER_SA_EMAIL"
+  plaintext_value  = data.google_service_account.github_provider_sa[each.key].email
+
+  depends_on = [github_repository.app-repository]
+}
