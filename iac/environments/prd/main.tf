22e49b587beb817d702f551e8e3dee2293d53933
X-I-A <develop@x-i-a.com>
2024-02-11 07:10:59 +0100

    feat: initialization

--- iac/environments/prd/main.tf
+++ iac/environments/prd/main.tf
@@ -1,3 +1,22 @@
+module "gcp-git" {
+  source = "../../modules/gcp-git"
+  project_id = var.project_id
+  repository_name = var.repository_name
+}
+
+module "gcp-wip" {
+  source = "../../modules/gcp-wip"
+  project_id = var.project_id
+  repository_owner = var.repository_owner
+  repository_name = var.repository_name
+}
+
+module "gcp-cbd" {
+  source = "../../modules/gcp-cbd"
+  project_id = var.project_id
+  repository_name = module.gcp-git.repository_name
+}
+
 module "github" {
   source = "../../modules/github"
   project_id = var.project_id
