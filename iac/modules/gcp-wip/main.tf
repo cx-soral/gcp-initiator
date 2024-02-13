0d61aed77cac2fac560872d67c39e8272bf31fdb
X-I-A <develop@x-i-a.com>
2024-02-13 20:49:23 +0100

    feat: update dependencies

--- iac/modules/gcp-wip/main.tf
+++ iac/modules/gcp-wip/main.tf
@@ -20,7 +20,7 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
   description  = "Provider for GitHub Actions of ${var.repository_name}"
 
    # Attribute mapping / condition from the OIDC token to Google Cloud attributes
-  attribute_condition = "assertion.repository == '${var.repository_owner}/${var.repository_name}' && assertion.ref == 'refs/heads/main'"
+  attribute_condition = "assertion.repository == '${var.repository_owner}/${var.repository_name}' && assertion.ref.matches('refs/heads/main')"
 
   attribute_mapping = {
     "google.subject" = "assertion.sub",
